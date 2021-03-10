3010fcb8a9856037b77e86b6817b49017c1596d4
Fernando <fcano@elpuig.xeill.net>
2021-03-10 18:19:18 +0100

    inicial

--- src/ex1/hashTableTest.java
+++ src/ex1/hashTableTest.java
@@ -20,30 +20,41 @@ class hashTableTest {
     }
 
     @org.junit.jupiter.api.Test
-    void put() {
+    void putcontar() {
         HashTable hashTable = new HashTable();
-        hashTable.put("test1","123");
-        hashTable.put("test2","231");
-        hashTable.put("test3","312");
-        hashTable.put("test4","1");
-        hashTable.put("test5","2");
-        hashTable.put("test6","3");
-        hashTable.put("test7","4");
-        hashTable.put("test8","5");
-        hashTable.put("test9","6");
-        //si la key tiene 6 digitos hash es negativo
-        hashTable.put("test","7");
+        hashTable.put("1","123");
+        hashTable.put("2","231");
+        hashTable.put("3","312");
+        hashTable.put("4","1");
+        hashTable.put("5","2");
+        hashTable.put("6","3");
+        hashTable.put("7","4");
+        hashTable.put("8","5");
+        hashTable.put("9","6");
+        hashTable.put("10","7");
+        hashTable.put("11","8");
+        hashTable.put("12","9");
+        hashTable.put("13","10");
+        hashTable.put("14","11");
+        hashTable.put("15","12");
+        hashTable.put("16","13");
+
+        hashTable.put("17","14");
+        //si la key tiene 6 digitos hash es negativo (teoria)
+        //Valor se duplica
 
         hashTable.count();
-        Assertions.assertEquals(10,hashTable.toString());
+        Assertions.assertEquals(16,hashTable.toString());
 
     }
 
     @org.junit.jupiter.api.Test
     void get() {
+
     }
 
     @org.junit.jupiter.api.Test
     void drop() {
+        //No puedes borrar algo que no existe
     }
 }
